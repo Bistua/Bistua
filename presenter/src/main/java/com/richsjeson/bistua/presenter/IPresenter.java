44b17903acf09509d681f07d9dd462f610ed249d
981213 <631015881213>
2016-04-19 11:00:22 +0800

    新增生命周期掌控

--- presenter/src/main/java/com/richsjeson/bistua/presenter/IPresenter.java
+++ presenter/src/main/java/com/richsjeson/bistua/presenter/IPresenter.java
@@ -1,7 +1,17 @@
 package com.richsjeson.bistua.presenter;
 
+import com.richsjeson.bistua.view.IView;
+
 /**
- * Created by Administrator on 2016/4/19.
+ * Created by RichsJeson on 2016/4/19.
+ * @see <p>设置Presenter的生命周期</p>
  */
-public interface IPresenter {
+public interface IPresenter<V extends IView> {
+
+    void onCreate();
+
+    void setView(V view);
+
+    void onDestroy();
+
 }
---
+++ view/src/main/java/com/richsjeson/bistua/view/IView.java
@@ -0,0 +1,7 @@
+package com.richsjeson.bistua.view;
+
+/**
+ * Created by Administrator on 2016/4/19.
+ */
+public interface IView {
+}
